b507badf1f95ea445c0e2ed36f9dd90f9bd2b364
weidinger.hp <weidinger.hp@gmail.com>
2021-09-04 00:31:39 +0200

    with top text and spawning viruses

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 mod virus;
 
-use bevy::{asset::Asset, prelude::*};
-use bevy_kira_audio::AudioPlugin;
+use bevy::{ecs::component::Component, prelude::*};
 use virus::VirusPlugin;
 use wasm_bindgen::prelude::*;
 
-struct BackgroundRessources {
-    background: Handle<ColorMaterial>,
+pub struct GameStatus {
+    infected: u16,
+    vaccinated: u16,
 }
+
+struct VaccinatedText;
+
+struct InfectedText;
 
 #[wasm_bindgen]
 pub fn run() {
@@ -23,7 +27,9 @@ pub fn run() {
     .add_plugins(DefaultPlugins)
     .add_plugin(bevy_kira_audio::AudioPlugin)
     .add_plugin(VirusPlugin)
-    .add_startup_system(game_init.system());
+    .add_startup_system(game_init.system())
+    .add_system(update_text_infected.system())
+    .add_system(update_text_vaccinated.system());
 
 
     #[cfg(target_arch = "wasm32")]
@@ -43,4 +49,80 @@ fn game_init(
         ..Default::default()
     });
 
+    commands.insert_resource(GameStatus {
+        infected: 0,
+        vaccinated: 0,
+    });
+
+    // At least we want two items that show the actual game status (points)
+    let font_handle: Handle<Font> = asset_server.load("ROBOTO-REGULAR.TTF");
+
+    let text_style = TextStyle {
+        font: font_handle,
+        font_size: 20.,
+        color: Color::BLACK,
+        ..Default::default()
+    };
+
+    create_text(&mut commands, Vec3::new(-310.,230.,2.), "Geimpft: ".to_owned(), TextAlignment {
+        horizontal: HorizontalAlign::Right,
+        vertical: VerticalAlign::Bottom,
+        ..Default::default()
+    }, &text_style, VaccinatedText);
+
+    create_text(&mut commands, Vec3::new(310.,230.,2.), "Infiziert: ".to_owned(), TextAlignment {
+        horizontal: HorizontalAlign::Left,
+        vertical: VerticalAlign::Bottom,
+        ..Default::default()
+    }, &text_style, InfectedText);
+}
+
+fn create_text<C>(commands: &mut Commands, translation:Vec3, title: String, alignment: TextAlignment, text_style: &TextStyle, component: C) where C: Component {
+    commands.spawn_bundle(Text2dBundle {
+        text: Text {
+            alignment: alignment,
+            sections: vec![TextSection {
+                value: title,
+                style: text_style.clone(),
+            },
+            TextSection {
+                value: "0".to_owned(),
+                style: text_style.clone(),
+            }
+            ]
+        },
+        transform: Transform {
+            translation: translation,
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+        .insert(component)
+        .insert(Timer::from_seconds(0.1, true)); // this timer is just to throttle the Graphical updates a little.
+}
+
+fn update_text_infected(
+    game_status: Res<GameStatus>, 
+    time: Res<Time>,
+    mut query: Query<(&mut Text, &mut Timer), With<InfectedText>>
+) {
+    if let Ok((mut text, mut timer)) = query.single_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            text.sections[1].value = game_status.infected.to_string();
+        }
+    }
+}
+
+fn update_text_vaccinated(
+    game_status: Res<GameStatus>, 
+    time: Res<Time>,
+    mut query: Query<(&mut Text, &mut Timer), With<VaccinatedText>>
+) {
+    if let Ok((mut text, mut timer)) = query.single_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            text.sections[1].value = game_status.vaccinated.to_string();
+        }
+    }
 }
--- src/virus.rs
+++ src/virus.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy_kira_audio::{AudioSource, Audio};
 
+use crate::GameStatus;
+
 pub struct VirusAssets {
     virus: Handle<TextureAtlas>,
     cough1: Handle<AudioSource>,
@@ -11,21 +13,24 @@ pub struct VirusSpawner;
 
 pub struct VirusParticle {
     is_hit:bool,
+    speed_x: f32,
+    speed_y: f32,
+    rotation: f32,
 }
 
 pub struct VirusPlugin;
 
 impl Plugin for VirusPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(init_virus.system());
-        app.add_system(spawn_viruses.system());
+        app.add_startup_system(init_virus.system())
+            .add_system(spawn_viruses.system())
+            .add_system(virus_fly_loop.system());
     }
 }
 
 fn init_virus(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     mut materialmap: ResMut<Assets<TextureAtlas>>,
 ) {
     let covid_map = asset_server.load("covid_map.png");
@@ -55,8 +60,57 @@ fn spawn_viruses(
         if timer.just_finished() {
             commands.entity(entity).insert(Timer::from_seconds(3., false));
 
+            // Create new Virus Entity
+            commands.spawn_bundle(SpriteSheetBundle {
+                texture_atlas: virus_assets.virus.clone(),
+                sprite: TextureAtlasSprite {
+                    index: 1,
+                    ..Default::default()
+                },
+                transform: Transform {
+                    translation: Vec3::new(320., 100., 1.),
+                    scale: Vec3::new(0.5, 0.5, 1.),
+                    ..Default::default()
+                },
+                ..Default::default()
+            }).insert(VirusParticle {
+                is_hit: false,
+                speed_y: -0.1,
+                speed_x: -1.,
+                rotation: 0.02,
+            }).insert(Timer::from_seconds(0.02, true));
+
             audio.play(virus_assets.cough1.clone());
         }
 
+    }
+}
+
+fn virus_fly_loop(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut gamestatus: ResMut<GameStatus>,
+    mut query: Query<(Entity, &mut Timer, &mut Transform, &VirusParticle), With<VirusParticle>>,
+
+) {
+    for (entity, mut timer, mut transform, virus) in query.iter_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            if virus.is_hit {
+                transform.translation.y -= 1.;
+                if transform.translation.y < -200. {
+                    commands.entity(entity).despawn();
+                }
+            } else {
+                transform.translation.x += virus.speed_x;
+                transform.translation.y += virus.speed_y;
+                transform.rotate(Quat::from_rotation_z(virus.rotation));
+
+                if transform.translation.x < -320. {
+                    commands.entity(entity).despawn();
+                    gamestatus.infected += 1;
+                }
+            }
+        }
     }
 }
